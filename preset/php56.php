93f24647a1510abbd4c47248d9cfea1f4a17276c
spulakk <krystof.spulak@modulservis.cz>
2021-02-01 14:37:33 +0100

    v2 rework

---
+++ preset/php56.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return function (Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator): void {
+	$containerConfigurator->import(__DIR__ . '/common/*.php');
+
+	$services = $containerConfigurator->services();
+};
