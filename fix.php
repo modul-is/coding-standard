701fbd5b4b3e1235caba2396df59f86ced1c3752
spulakk <krystof.spulak@modulservis.cz>
2022-01-21 10:03:12 +0100

    Update fix.php

--- fix.php
+++ fix.php
@@ -92,7 +92,7 @@ file_put_contents($fileList, implode("\n", iterator_to_array($finder)));
 // PHP CS Fixer
 passthru(
 	'php ' . escapeshellarg($vendorDir . '/friendsofphp/php-cs-fixer/php-cs-fixer')
-	. ' fix -v'
+	. ' fix -v --diff'
 	. ($dry ? ' --dry-run' : '')
 	. ' --config ' . escapeshellarg(__DIR__ . "/preset-fixer/$preset.php"),
 	$code
