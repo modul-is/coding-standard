87aa930c7c843b30713bc1a9baf6cf64e666ed96
spulakk <krystof.spulak@modulservis.cz>
2023-05-19 13:11:34 +0200

    named argument space fixer

---
+++ src/Fixer/NamedArgumentSpaceFixer.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ModulIS\Fixer\Basic;
+
+use PhpCsFixer\AbstractFixer;
+use PhpCsFixer\FixerDefinition\CodeSample;
+use PhpCsFixer\FixerDefinition\FixerDefinition;
+use PhpCsFixer\FixerDefinition\FixerDefinitionInterface;
+use PhpCsFixer\Preg;
+use PhpCsFixer\Tokenizer\Token;
+use PhpCsFixer\Tokenizer\Tokens;
+
+final class NamedArgumentSpaceFixer extends AbstractFixer
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getDefinition(): FixerDefinitionInterface
+	{
+		return new FixerDefinition(
+			'There must be a space between a named argument and its value.',
+			[
+				[new CodeSample('<?php
+$this->method(argument:1);
+')]
+			]
+		);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 *
+	 * Must run after NoTrailingWhitespaceFixer
+	 */
+	public function getPriority(): int
+	{
+		return -1;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function isCandidate(Tokens $tokens): bool
+	{
+		return true;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function applyFix(\SplFileInfo $file, Tokens $tokens): void
+	{
+		$content = $tokens->generateCode();
+
+		$newContent = Preg::replace('/(\w+:)(\w+)/', '$1 $2', $content);
+
+		$newTokens = Tokens::fromCode($newContent);
+
+		foreach($newTokens as $index => $token)
+		{
+			$newTokens[$index] = new Token($token->getContent());
+		}
+
+		$tokens->overrideRange(0, $tokens->count() - 1, $newTokens);
+	}
+
+
+	public function getName(): string
+	{
+		return 'ModulIS/' . parent::getName();
+	}
+}
